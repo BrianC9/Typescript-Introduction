48b0cdf032385f01bd2cae1a957ef173438f8bcc
Bryan Cusme <bryan.cusme@devoteam.com>
2022-12-18 21:09:11 +0100

    typescript set up with debugger

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let nameS = "Bryan";
+let age = 18;
+let isInLegallAge = false;
+if (age <= 18) {
+    nameS += " +18";
+    isInLegallAge = true;
+}
+console.log(nameS, isInLegallAge);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let nameS: String = "Bryan";
+let age:number = 18;
+let isInLegallAge:boolean=false;
+
+if (age <= 18){
+    nameS += " +18"
+    isInLegallAge = true;
+}
+console.log(nameS, isInLegallAge);
