7a13959bdd908f14de6312d9e5d7f46eecc1f6c7
Bryan Octavio <75798156+BrianC9@users.noreply.github.com>
2022-12-20 12:04:20 +0100

    Delete dist directory

--- dist/intersectionTypes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let textBox = {
-    drag: () => { },
-    resize: () => { }
-};
-//# sourceMappingURL=intersectionTypes.js.map
--- dist/literalTypes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let quantity = 50;
-let quantity2 = 100;
-let unit = 'inch';
-//# sourceMappingURL=literalTypes.js.map
